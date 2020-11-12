787d4e121c942ca0f6669938822e144e25b85b79
Baeyezin <bam1991@naver.com>
2020-11-12 18:30:11 +0900

    ScrolltoTop Controller 제작 - 배예진
    ScrolltoTop Controller 제작
    
    20-11-12 18:30

---
+++ src/main/java/org/zerock/controller/ScrollController.java
@@ -0,0 +1,19 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@RequestMapping("/")
+@Log4j
+@Controller
+public class ScrollController {
+	@GetMapping("scrolltotop")
+	public void scroll() {
+		
+	}
+	
+
+}
